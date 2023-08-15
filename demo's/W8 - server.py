3ce7dc630feec327f42541e9a9215860c52e116a
Gtop99 <137779909+Gtop99@users.noreply.github.com>
2023-08-15 10:24:13 +0100

    Initial commit

---
+++ demo's/W8 - server.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+def coffee_sleep(n=5):
+    coffee = []
+    sleep = []
+    for i in range(n):
+        coffee.append(int(input("Enter amount of coffee you drink per week: ")))
+        sleep.append(int(input("Enter average sleep duration per night: ")))
+    return coffee, sleep
+
+
+#print(coffee_sleep())
+
+def movies(n=5):
+    movie = {}
+    for i in range(n):
+        genre = input("Enter your favourite movie genre: ")
+        movie[genre] = movie.get(genre, 0) + 1
